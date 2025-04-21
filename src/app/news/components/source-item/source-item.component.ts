b32d5e8ed9dba59ba2841423c9e760667b66b706
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-21 12:54:03 -0500

    feat(sources): add source item component.

---
+++ src/app/news/components/source-item/source-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Source} from '../../model/source.entity';
+import {MatListItem, MatListItemAvatar, MatListItemLine} from '@angular/material/list';
+
+@Component({
+  selector: 'app-source-item',
+  imports: [
+    MatListItem,
+    MatListItemAvatar,
+    MatListItemLine
+  ],
+  templateUrl: './source-item.component.html',
+  styleUrl: './source-item.component.css'
+})
+export class SourceItemComponent {
+  @Input() source!: Source;
+  @Output() sourceSelected = new EventEmitter<Source>();
+
+  onClick() {
+    this.sourceSelected.emit(this.source);
+  }
+}
