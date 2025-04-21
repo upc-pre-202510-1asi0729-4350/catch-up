fc18764b97bc691c2ed93cbbf506f16ee5dc30ea
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-21 10:57:07 -0500

    feat(i18n): add translation files and translate loader configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// NGX Translate Loader Factory
+const httpLoaderFactory: (http: HttpClient) => TranslateLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en',
+    })]
 };
