a105870f7e01fae97457a32b271a988d0bb06509
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-21 11:41:21 -0500

    feat(sources): add logo api service client.

---
+++ src/app/shared/services/logo-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Source} from '../../news/model/source.entity';
+
+@Injectable({providedIn: 'root'})
+export class LogoApiService {
+  baseUrl = environment.logoProviderApiBaseUrl;
+
+  constructor() { }
+
+  getUrlToLogo(source: Source) {
+    console.log('getUrlToLogo', source);
+    return `${this.baseUrl}${new URL(source.url).hostname}`;
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+};
