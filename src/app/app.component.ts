69bae8f1b11c9f8058de769e21f98b7d65f0c3e7
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-23 10:24:02 -0500

    feat(main): implement main behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
+import {SideNavigationBarComponent} from './public/components/side-navigation-bar/side-navigation-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    SideNavigationBarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'catch-up';
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
