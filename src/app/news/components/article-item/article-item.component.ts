d9bd72cde6604c6c4796e68252dedad4614c88d7
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-23 09:46:42 -0500

    feat(articles): add article item component behavior and structure.

--- src/app/news/components/article-item/article-item.component.ts
+++ src/app/news/components/article-item/article-item.component.ts
@@ -1,10 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {Article} from '../../model/article.entity';
 import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardTitleGroup
+} from '@angular/material/card';
+import {DatePipe} from '@angular/common';
+import {MatAnchor, MatIconButton} from '@angular/material/button';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatIcon} from '@angular/material/icon-module.d-BeibE7j0';
 
 @Component({
   selector: 'app-article-item',
-  imports: [MatSnackBarModule],
+  imports: [MatSnackBarModule, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatCardImage, MatCardTitleGroup, DatePipe, MatAnchor, TranslatePipe, MatIconButton, MatIcon],
   templateUrl: './article-item.component.html',
   styleUrl: './article-item.component.css'
 })
