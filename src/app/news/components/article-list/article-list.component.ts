1fcfd0334d73a569d415d259e7c2be8ded31f3f0
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-23 09:53:16 -0500

    feat(articles): add article list component.

---
+++ src/app/news/components/article-list/article-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {Article} from '../../model/article.entity';
+import {ArticleItemComponent} from '../article-item/article-item.component';
+
+@Component({
+  selector: 'app-article-list',
+  imports: [
+    ArticleItemComponent
+  ],
+  templateUrl: './article-list.component.html',
+  styleUrl: './article-list.component.css'
+})
+export class ArticleListComponent {
+  @Input() articles: Array<Article> = [];
+
+}
